ba1a204fdc1e87f78e6e234a1e4ca0eb09ab1e72
changvvb <changvvb@gmail.com>
2020-07-25 00:53:07 +0800

    Update Scalable

--- pbconverts/src/main/scala-2.12/pbconverts/ScalableImplicits.scala
+++ pbconverts/src/main/scala-2.12/pbconverts/ScalableImplicits.scala
@@ -1,52 +1,11 @@
 package pbconverts
 
-import java.time.{Instant, ZoneId, ZonedDateTime}
-
-import com.google.protobuf.{BoolValue, DoubleValue, FloatValue, Int32Value, Int64Value, StringValue, Timestamp}
-
 import scala.collection.JavaConverters._
 import scala.collection.TraversableLike
 import scala.collection.generic.CanBuildFrom
 import scala.reflect.ClassTag
 
-trait Scalable[+T, -M] {
-  def toScala(proto: M): T
-}
-
-object Scalable {
-
-  def apply[T <: Product, M]: Scalable[T, M] = macro ProtoScalableMacro.scalasImpl[T, M]
-
-  def apply[T, M](convert: M ⇒ T): Scalable[T, M] =
-    new Scalable[T, M] {
-      override def toScala(proto: M): T = convert(proto)
-    }
-
-  implicit val javaIntegerScalable = Scalable[Int, java.lang.Integer](_.toInt)
-  implicit val javaLongScalable = Scalable[Long, java.lang.Long](_.toLong)
-  implicit val javaDoubleScalable = Scalable[Double, java.lang.Double](_.toDouble)
-  implicit val javaFloatScalable = Scalable[Float, java.lang.Float](_.toFloat)
-  implicit val javaCharacterScalable = Scalable[Char, java.lang.Character](_.toChar)
-  implicit val javaByteScalable = Scalable[Byte, java.lang.Byte](_.toByte)
-
-  implicit val stringValueScalable = Scalable[String, StringValue](_.getValue)
-  implicit val int32ValueScalable = Scalable[Int, Int32Value](_.getValue)
-  implicit val int64ValueScalable = Scalable[Long, Int64Value](_.getValue)
-  implicit val doubleValueScalable = Scalable[Double, DoubleValue](_.getValue)
-  implicit val floatValueScalable = Scalable[Float, FloatValue](_.getValue)
-  implicit val boolValueScalable = Scalable[Boolean, BoolValue](_.getValue)
-
-  implicit val zonedDateTimeProtoable = Scalable[ZonedDateTime, Timestamp] { proto ⇒
-    Instant.ofEpochSecond(proto.getSeconds, proto.getNanos).atZone(ZoneId.systemDefault())
-  }
-
-  // M => Option[T]
-  implicit def optScalable[T, M](implicit scalable: Scalable[T, M]): Scalable[Option[T], M] =
-    Scalable { proto ⇒
-      Option(scalable.toScala(proto))
-    }
-
-  implicit def optScalable2[T]: Scalable[Option[T], T] = Scalable(Option(_))
+trait ScalableImplicits {
 
   // java.lang.Iterable[M] => That[T]
   implicit def iterableScalable[That[_], T, M](implicit
--- pbconverts/src/main/scala-2.13/pbconverts/ScalableImplicits.scala
+++ pbconverts/src/main/scala-2.13/pbconverts/ScalableImplicits.scala
@@ -1,54 +1,12 @@
 package pbconverts
 
-import java.lang
-import java.time.{Instant, ZoneId, ZonedDateTime}
-
-import com.google.protobuf.{BoolValue, DoubleValue, FloatValue, Int32Value, Int64Value, StringValue, Timestamp}
-
 import scala.collection.BuildFrom
 import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
 
-trait Scalable[+T, -M] {
-  def toScala(proto: M): T
-}
+trait ScalableImplicits {
 
-object Scalable {
-
-  def apply[T <: Product, M]: Scalable[T, M] = macro ProtoScalableMacro.scalasImpl[T, M]
-
-  def apply[T, M](convert: M ⇒ T): Scalable[T, M] =
-    new Scalable[T, M] {
-      override def toScala(proto: M): T = convert(proto)
-    }
-
-  implicit val javaIntegerScalable: Scalable[Int, Integer] = Scalable(_.toInt)
-  implicit val javaLongScalable: Scalable[Long, lang.Long] = Scalable(_.toLong)
-  implicit val javaDoubleScalable: Scalable[Double, lang.Double] = Scalable(_.toDouble)
-  implicit val javaFloatScalable: Scalable[Float, lang.Float] = Scalable(_.toFloat)
-  implicit val javaCharacterScalable: Scalable[Char, Character] = Scalable(_.toChar)
-  implicit val javaByteScalable: Scalable[Byte, lang.Byte] = Scalable(_.toByte)
-
-  implicit val stringValueScalable: Scalable[String, StringValue] = Scalable(_.getValue)
-  implicit val int32ValueScalable: Scalable[Int, Int32Value] = Scalable(_.getValue)
-  implicit val int64ValueScalable: Scalable[Long, Int64Value] = Scalable(_.getValue)
-  implicit val doubleValueScalable: Scalable[Double, DoubleValue] = Scalable(_.getValue)
-  implicit val floatValueScalable: Scalable[Float, FloatValue] = Scalable(_.getValue)
-  implicit val boolValueScalable: Scalable[Boolean, BoolValue] = Scalable(_.getValue)
-
-  implicit val zonedDateTimeProtoable: Scalable[ZonedDateTime, Timestamp] = Scalable { proto ⇒
-    Instant.ofEpochSecond(proto.getSeconds, proto.getNanos).atZone(ZoneId.systemDefault())
-  }
-
-//   M => Option[T]
-  implicit def optScalable[T, M](implicit scalable: Scalable[T, M]): Scalable[Option[T], M] =
-    Scalable { proto ⇒
-      Option(scalable.toScala(proto))
-    }
-
-  implicit def optScalable2[T]: Scalable[Option[T], T] = Scalable(Option(_))
-
-//   java.lang.Iterable[M] => That[T]
+  //   java.lang.Iterable[M] => That[T]
   implicit def iterableScalable[That[_], T, M](implicit
       scalable: Scalable[T, M],
       bf: BuildFrom[Seq[_], T, That[T]]
@@ -62,7 +20,7 @@ object Scalable {
       bf.fromSpecific(Seq.empty)(proto.asScala)
     }
 
-//   java.lang.Iterable[M] => Array[T]
+  //   java.lang.Iterable[M] => Array[T]
   implicit def arrayScalable[T: ClassTag, M](implicit scalable: Scalable[T, M]): Scalable[Array[T], java.lang.Iterable[M]] =
     Scalable { proto ⇒
       proto.asScala.map(scalable.toScala).toArray
@@ -70,7 +28,7 @@ object Scalable {
 
   implicit def arrayScalable2[T: ClassTag]: Scalable[Array[T], java.lang.Iterable[T]] = Scalable { proto ⇒ proto.asScala.toArray }
 
-//  // Repr[M] => That[T]
+  //  // Repr[M] => That[T]
   implicit def iterableSelfScalable[That, Repr <: Iterable[M], T, M](implicit
       scalable: Scalable[T, M],
       bf: BuildFrom[Iterable[_], T, That]
@@ -91,5 +49,4 @@ object Scalable {
 
   implicit def mapScalable1[K, V]: Scalable[Map[K, V], java.util.Map[K, V]] =
     Scalable[Map[K, V], java.util.Map[K, V]] { m ⇒ m.asScala.toMap }
-
 }
