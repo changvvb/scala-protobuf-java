84a744c1a34e33275c8dd178379097089f21a92a
changvvb <changvvb@gmail.com>
2020-09-24 19:46:13 +0800

    Add test

--- pbconverts/src/test/scala/pbconverts/MapSpec.scala
+++ pbconverts/src/test/scala/pbconverts/MapSpec.scala
@@ -1,7 +1,8 @@
 package pbconverts
 
+import com.google.protobuf.ByteString
 import org.scalatest.funsuite.AnyFunSuite
-import pbconverts.ConversionTest.TestMessageDto
+import pbconverts.ConversionTest.{PBTestBytes, TestMessageDto}
 
 class MapSpec extends AnyFunSuite {
   test("test map") {
@@ -15,4 +16,20 @@ class MapSpec extends AnyFunSuite {
     assert(testMessageDto.getStringStringKVOrThrow("string1") == "string1")
   }
 
+  case class TestBytes(m: Map[String, Int])
+  test("test conversion between bytes in protobuf and Map in case class") {
+    val pbTest1 = ProtoableBuilder[TestBytes, PBTestBytes]
+      .setField(_.getTestBytes, x => ByteString.copyFromUtf8(x.m.keys.mkString(",")))
+      .build
+      .toProto(TestBytes(Map("test" -> 1, "bytes" -> 1)))
+
+    assert(pbTest1.getTestBytes.toStringUtf8 == "test,bytes")
+
+    val test = ScalableBuilder[TestBytes, PBTestBytes]
+      .setField(_.m, _.getTestBytes.toStringUtf8.split(",").map(_ -> 1).toMap)
+      .build
+      .toScala(PBTestBytes.newBuilder().setTestBytes(ByteString.copyFromUtf8("test,bytes")).build())
+
+    assert(test.m == Map("test" -> 1, "bytes" -> 1))
+  }
 }
